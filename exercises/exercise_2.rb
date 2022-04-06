f21ec5d1447fb2210d5a6d86a8cf8dfacdc62233
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-06 14:10:02 -0400

    solve exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,20 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+class StoreUpdate
+
+  def initialize
+    @store1 = Store.find_by(id: 1)
+    @store2 = Store.find_by(id: 2)
+  end
+
+  def change_store1_info
+    @store1.name = "Sudbury"
+    @store1.save
+  end
+
+end
+
+store_update = StoreUpdate.new
+store_update.change_store1_info
