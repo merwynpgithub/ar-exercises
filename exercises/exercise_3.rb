a865c6676b5318d1cedb8c4c7c434bd6ca9012f0
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-06 14:21:42 -0400

    solve exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,25 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+
+puts "Initial count before change"
+puts Store.all.count
+
+class StoreUpdate2
+
+  def initialize
+    @store3 = Store.find_by(id: 3)
+  end
+
+  def erase_store3
+    @store3.destroy
+  end
+
+end
+
+store_update2 = StoreUpdate2.new
+store_update2.erase_store3
+
+puts "Current count after change"
+puts Store.all.count
