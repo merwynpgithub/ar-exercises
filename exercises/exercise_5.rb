0182ae325bae7902ea8e584fe9dbf4a23c612731
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-06 14:53:17 -0400

    solve exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Calculating total revenue for all stores"
+puts Store.sum(:annual_revenue)
+
+puts "Calculating average revenue"
+puts Store.sum(:annual_revenue)/Store.all.count
+
+puts "Calculating number of stores that are generating $1M or more"
+puts Store.where("annual_revenue > ?", 1000000).all.count
