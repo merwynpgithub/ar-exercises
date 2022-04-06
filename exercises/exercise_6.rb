b85f6a29096a0f27a441e5decbff206257c4fc54
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-06 15:20:48 -0400

    solve exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+employee1 = Employee.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+employee2 = Employee.create(first_name: "John", last_name: "Samuel", hourly_rate: 45)
+
+employee1.update(store_id: 4, first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+employee2.update(store_id: 6, first_name: "John", last_name: "Samuel", hourly_rate: 45)
